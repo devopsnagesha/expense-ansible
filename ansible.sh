2fbca953213c8b3df63c19424240815faecd3f17
nagesha technology <asbconstruction19@gmail.com>
2024-04-06 13:29:02 +0530

    Adding gocd encrypt in pipe line and made changes in vault_token

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,5 @@
 component=$1
 
-ansible-playbook -i $component-$env.angadicnc.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=$env -e role_name=$component expense.yml
+ansible-playbook get-secrets.yml -e vault_token=$vault_token -e env=$env -e role_name=$component
+ansible-playbook -i $component-$env.angadicnc.online, -e env=$env -e role_name=$component expense.yml -e '@~/secrets.json' -e '@~/app.json'
+rm -f ~/secrets.json ~/app.json
